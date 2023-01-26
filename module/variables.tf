0d2bd3cce1a5fbddf7008fa4f6925fb63015c1b1
BMeyn <50145395+BMeyn@users.noreply.github.com>
2023-01-26 20:17:54 +0100

    Update variables.tf

--- module/variables.tf
+++ module/variables.tf
@@ -2,3 +2,8 @@ variable "rg_name" {
   type        = string
   description = "The resource group name"
 }
+
+variable "rg_location" {
+  type        = string
+  description = "Location of the resource group"
+}
